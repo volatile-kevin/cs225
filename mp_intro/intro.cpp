06b0185a77f603e99fc2d543abf8fa14d4a418d4
Kevin Hu <kwh2@linux-04.ews.illinois.edu>
2019-09-06 17:35:40 -0500

    mandelbrot update

--- mp_intro/intro.cpp
+++ mp_intro/intro.cpp
@@ -108,17 +108,78 @@ void rotate(std::string inputFile, std::string outputFile) {
 cs225::PNG myArt(unsigned int width, unsigned int height) {
   cs225::PNG png(width, height);
   // TODO: Part 3
-  for (int x = 0; x < width; x++) {
-    for (int y = 0; y < height; y++) {
-      cs225::HSLAPixel & pixel = png.getPixel(x, y);
-      pixel.h = rand() % 360;
-      pixel.s = 0.75;
-      pixel.l = 0.5;
-      pixel.a = 0.5;
+  double MinRe = -2.0;
+double MaxRe = 1.0;
+double MinIm = -1.2;
+double MaxIm = MinIm+(MaxRe-MinRe)*height/width;
+double Re_factor = (MaxRe-MinRe)/(width-1);
+double Im_factor = (MaxIm-MinIm)/(height-1);
+unsigned MaxIterations = 100;
+
+for(unsigned y=0; y<height; ++y)
+{
+    double c_im = MaxIm - y*Im_factor;
+    for(unsigned x=0; x<width; ++x)
+    {
+        double c_re = MinRe + x*Re_factor;
+
+        double Z_re = c_re, Z_im = c_im;
+        bool isInside = true;
+
+        for(unsigned n = 0; n<MaxIterations; ++n)
+        {
+            double Z_re2 = Z_re*Z_re, Z_im2 = Z_im*Z_im;
+            if(Z_re2 + Z_im2 > 4)
+            {
+                isInside = false;
+                break;
+            }
+            Z_im = 2*Z_re*Z_im + c_im;
+            Z_re = Z_re2 - Z_im2 + c_re;
+
+        }
+
+        if(isInside) {
+          cs225::HSLAPixel & pixel = png.getPixel(x, y);
+           pixel.h = 240;
+           pixel.s = 1;
+           pixel.l = .45;
+           pixel.a = 1;
+         }
+        if(!isInside){
+          cs225::HSLAPixel & pixel = png.getPixel(x, y);
+           pixel.h = 1;
+           pixel.s = 1;
+           pixel.l = 1;
+           pixel.a = 1;
+        }
 
     }
+}
+for(unsigned a = 0; a<width-1; ++a){
+  for(unsigned b = 0; b<height-1; ++b){
+    cs225::HSLAPixel & pixel = png.getPixel(a, b);
+    cs225::HSLAPixel & pixel1 = png.getPixel(a+1, b);
+    cs225::HSLAPixel & pixel2 = png.getPixel(a, b+1);
+    cs225::HSLAPixel & pixel3 = png.getPixel(a+1, b+1);
+    cs225::HSLAPixel & pixel4 = png.getPixel(a-1, b);
+    cs225::HSLAPixel & pixel5 = png.getPixel(a, b-1);
+    cs225::HSLAPixel & pixel6 = png.getPixel(a-1, b-1);
+    cs225::HSLAPixel & pixel7 = png.getPixel(a+1, b-1);
+    cs225::HSLAPixel & pixel8 = png.getPixel(a-1, b+1);
+
+    if(pixel1.l != .45 && pixel2.l != .45 && pixel3.l != .45 && pixel4.l != .45){
+      pixel.l = 0;
+    }
+
+
 
   }
+}
+//Code based on http://warp.povusers.org/Mandelbrot/
+
+
+
 
   return png;
 }

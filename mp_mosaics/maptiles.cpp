f126ada95193d28a3d19a36473cbc466b700d0fc
Kevin Hu <kwh2@linux-07.ews.illinois.edu>
2019-10-29 21:19:18 -0500

    part 3 done

--- mp_mosaics/maptiles.cpp
+++ mp_mosaics/maptiles.cpp
@@ -21,7 +21,35 @@ MosaicCanvas* mapTiles(SourceImage const& theSource,
     /**
      * @todo Implement this function!
      */
+     vector<Point<3>> tilePoints;
+     MosaicCanvas *canvas = new MosaicCanvas(theSource.getRows(), theSource.getColumns());
+     int rows = (int)canvas->getRows();
+     int cols = (int)canvas->getColumns();
 
-    return NULL;
+     map<Point<3>, TileImage*> map_tiles;
+
+     size_t i = 0;
+     while(i < theTiles.size()){
+       LUVAPixel p = theTiles[i].getAverageColor();
+
+       Point<3> pTile = convertToXYZ(p);
+       tilePoints.push_back(pTile);
+       map_tiles[pTile] = &theTiles[i];
+       i++;
+     }
+
+     KDTree<3> new_tree(tilePoints);
+
+     for(int x = 0; x < rows; x++){
+       for(int y = 0; y < cols; y++){
+         int pos = x * cols + y;
+         LUVAPixel source_color = theSource.getRegionColor(x, y);
+         Point<3> source_point = convertToXYZ(source_color);
+
+         Point<3> close = new_tree.findNearestNeighbor(source_point);
+         canvas->setTile(x, y, map_tiles[close]);
+       }
+     }
+
+    return canvas;
 }
-
